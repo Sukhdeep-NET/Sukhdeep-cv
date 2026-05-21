4189da5f368b714c4c62d2b0234a76c8ea0ba301
Sukhdeep-NET <deepforweb@gmail.com>
2026-05-21 21:36:03 +0530

    Add theme toggle functionality with icon change

--- script.js
+++ script.js
@@ -119,3 +119,19 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 });
+const themeToggle = document.getElementById("theme-toggle");
+
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+
+    // Icon Change
+    const icon = themeToggle.querySelector("i");
+
+    if (document.body.classList.contains("dark-theme")) {
+        icon.classList.remove("fa-moon");
+        icon.classList.add("fa-sun");
+    } else {
+        icon.classList.remove("fa-sun");
+        icon.classList.add("fa-moon");
+    }
+});
